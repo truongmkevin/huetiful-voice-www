7417542a30332b6a0a6247cd889278d8296f16a9
Kevin Truong <truongmkevin@yahoo.com>
2020-03-21 02:42:01 -0500

    added router and tailwindcss for basic setup

---
+++ src/pages/Home/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div className="py-6">
+      <h1 className="text-6xl text-center text-indigo-700 font-bold">Huetiful Voice</h1>
+    </div>
+  )
+}
+
+export default Home;
